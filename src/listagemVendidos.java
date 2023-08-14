c0200074a82266b7d80e671bce601e11d055de3b
YasmineCruz92 <yasmineramalhocruz@gmail.com>
2023-08-13 21:38:55 -0300

    ajuste

--- src/listagemVendidos.java
+++ src/listagemVendidos.java
@@ -29,7 +29,7 @@ public class listagemVendidos extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "PRODUTO", "VALOR", "STATUS"
             }
         ));
         tabelaProdutosvendidos.addAncestorListener(new javax.swing.event.AncestorListener() {
